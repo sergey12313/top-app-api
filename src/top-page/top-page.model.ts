67e96dac82d49596fe5460ccc535057c8cc6984f
Sergey Razuvaev <sergey12313@gmail.com>
2022-01-28 14:31:12 +0300

    create project structure

---
+++ src/top-page/top-page.model.ts
@@ -0,0 +1,26 @@
+export enum TopLevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+
+export class TopPageModel {
+  firstLevel: TopLevelCategory;
+  secondCategory: string;
+  title: string;
+  category: string;
+  hh?: {
+    count: number;
+    juniorSalary: number;
+    middleSalary: number;
+    seniorSalary: number;
+  };
+  advantages: Array<{
+    title: string;
+    description: string;
+  }>;
+  seoText: string;
+  tagsTitles: string;
+  tags: Array<string>;
+}
